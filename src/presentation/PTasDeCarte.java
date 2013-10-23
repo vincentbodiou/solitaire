ceaa3e8e8d429bee34066487db500e4b4f66b224
vincent <vincent.bodiou@gmail.com>
2013-10-23 18:23:23 +0200

    PTas de carte

--- src/presentation/PTasDeCarte.java
+++ src/presentation/PTasDeCarte.java
@@ -1,16 +1,27 @@
 package presentation;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+enum Decalage {
+	HAUT,BAS,GAUCHE,DROITE
+}
 
 public class PTasDeCarte extends JPanel{
 
-	private static int dx=10,dy=10;
+	private static int PAS=PCarte.largeur*30/100;
+
+	private int dx = 0;
+	private int dy = 0;
 	
 	public PTasDeCarte() {
-		setLayout(null);
-		setSize(72, 96);
+		setLayout(null);		
+		setSize(PCarte.largeur, PCarte.hauteur);
+		setPreferredSize(getSize());
 		setBackground(Color.blue);
 		repaint();
 	}
@@ -18,7 +29,8 @@ public class PTasDeCarte extends JPanel{
 	public void empiler(PCarte carte) {
 		this.setSize(carte.getWidth() + this.getComponentCount() * dx ,carte.getHeight() + this.getComponentCount() * dy);
 		carte.setLocation(this.getComponentCount() * dx ,this.getComponentCount() * dy);
-		add(carte);		
+		setPreferredSize(getSize());
+		add(carte,0);
 		repaint();
 	}
 
@@ -27,5 +39,58 @@ public class PTasDeCarte extends JPanel{
 		remove(carte);
 		repaint();
 	}
+	
+	public void setDirectionDecalage(Decalage d){
+		
+		switch (d){
+			case HAUT :
+				dy = -(PTasDeCarte.PAS);
+				break;
+			case BAS :
+				dy = (PTasDeCarte.PAS);
+				break;
+			case GAUCHE :
+				dx = -(PTasDeCarte.PAS);
+				break;
+			case DROITE :
+				dx = PTasDeCarte.PAS;
+				break;
+		
+		}
+		
+	}
+	
+	/**
+     * programme de test : à déplacer dans une classe dédiée aux tests
+     * @param args
+     */
+    public static void main (String args []) {
+		JFrame f = new JFrame ("Test PCarte") ;
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setLayout(new FlowLayout()); // au lieu de BorderLayout par défaut
+		f.getContentPane ().setBackground(new Color(143, 143, 195)); // violet pâle
+	
+		PTasDeCarte tasDeCarte = new PTasDeCarte();
+		tasDeCarte.setVisible(true);
+				
+		// une carte visible
+		PCarte pc = new PCarte ("1C");
+		pc.setFaceVisible(true);
+		tasDeCarte.empiler(pc);
+			
+		pc = new PCarte("1D");
+		pc.setFaceVisible(true);
+		tasDeCarte.empiler(pc);
+		
+		pc = new PCarte("1D");
+		pc.setFaceVisible(true);
+		tasDeCarte.empiler(pc);	
+		
+		f.getContentPane ().add(tasDeCarte) ;	
+		
+		f.pack () ;		// dimensionner le cadre
+		f.setLocation(200,100);	// le positionner
+		f.setVisible (true) ;	// et le rendre visible
+    } // main
 
 }
